13431c84f2c959c93bcc287bfa6bc99b436b909e
Deye Lei <dleipersonal@gmail.com>
2022-09-19 23:33:19 -0400

    update asynchronous call with swift 5.5 syntax Task
    Embedded the old save load calls with new one with task.
    New lifecycle modifier .task{} were used instead of onAppear{}

--- MyApp.swift
+++ MyApp.swift
@@ -7,28 +7,24 @@ struct MyApp: App {
         WindowGroup {
             NavigationView{
                 ScrumsView(scrums: $store.scrums){
-                    ScrumStore.save(scrums: store.scrums){
-                        result in 
-                        if case .failure(let error) = result{
-                            fatalError(error.localizedDescription)
+                    Task{
+                        do{
+                            try await ScrumStore.save(scrums: store.scrums)
                         }
-                        //print("data save")
+                        catch{
+                            fatalError("fail to save")
+                        }
                     }
                 }
                     .navigationTitle("Daily Scrums")
             }
-            .onAppear(){
-                ScrumStore.load(completion: {
-                    result in 
-                    switch result{
-                        case .failure(let error):
-                        fatalError(error.localizedDescription)
-                        
-                        case .success(let scrums):
-                        store.scrums = scrums
-                    }
-                    //print("data load")
-                })
+            .task {
+                do{
+                    store.scrums = try await ScrumStore.load()
+                }
+                catch{
+                    fatalError("fail to load")
+                }
             }
         }
     }
--- ScrumStore.swift
+++ ScrumStore.swift
@@ -7,7 +7,7 @@ class ScrumStore: ObservableObject {
     
     /* use the fileManager to locate the document directory(.documentDirectory) in user's home directory(.userDomainMask)
      
-     return the URL with scrums.data appended to it
+     return the URL with scrums.data(it doesn't matter what name this is) appended to it
      */
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory,
@@ -16,6 +16,22 @@ class ScrumStore: ObservableObject {
                                     create: false)
         .appendingPathComponent("scrums.data")
     }
+    /*Embeded old load function with await syntax in swift 5.5
+     withCheckedThrowingContnuation call the supply closure with continuation instance which have method to throw a error or return a value to stop the suspansion on task
+     */
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                }
+            }
+        }
+    }
+    
     /* Creating an asynchrous task that submit to dispatchQueue(FIFO) in lowest priority(.background)
      
      the task call completion handler with decoded scrumdata or error message on condition
@@ -24,6 +40,10 @@ class ScrumStore: ObservableObject {
         DispatchQueue.global(qos: .background).async{
             do{
                 let fileURL = try fileURL()
+                /*
+                print(FileManager.default.fileExists(atPath: fileURL.path))
+                print("from load")
+                 */
                 guard let file = try? FileHandle(forReadingFrom: fileURL) else{
                     DispatchQueue.main.async{
                         completion(.success([]))
@@ -43,16 +63,41 @@ class ScrumStore: ObservableObject {
             }
         }
     }
+    /*
+     async call to old save function with same trick
+     */
+    @discardableResult
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedThrowingContinuation { continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrumsSaved):
+                    continuation.resume(returning: scrumsSaved)
+                }
+            }
+        }
+    }
     
     /*
      encode an array of scrum and save it into document directory in user's home directory
+     Notice scrum.data in document directory(URL) doesn't exit before the data.write(), and data.write create the file on first run and write to it. This function was trigged by lifecycle modifier of onChange{} which can be trigger by coming back to home screen. Thus, returning to home screen save the data in scrum.data 
      */
     static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
         DispatchQueue.global(qos: .background).async {
             do {
                 let data = try JSONEncoder().encode(scrums)
                 let outfile = try fileURL()
+                /*
+                print(FileManager.default.fileExists(atPath: outfile.path))
+                print("from save before write")
+                */
                 try data.write(to: outfile)
+                /*
+                print(FileManager.default.fileExists(atPath: outfile.path))
+                print("from save after write")
+                */
                 DispatchQueue.main.async {
                     completion(.success(scrums.count))
                 }
